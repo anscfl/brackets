560b8d5f05c57b2fdb49ce033f47193cd233c80c
anscfl <60544277+anscfl@users.noreply.github.com>
2020-03-21 17:22:22 +0400

    fix: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsArr = str.split('');
+
+    for (let i = 0; i < bracketsArr.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (bracketsConfig[j][0] === bracketsArr[i] && bracketsConfig[j][1] === bracketsArr[i + 1]) {
+                bracketsArr.splice(i, 2);
+                i = -1;
+                continue;
+            }
+        }
+    }
+    return bracketsArr.length === 0 ? true : false;
 }
